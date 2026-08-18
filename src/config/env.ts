1a8dd94ccf0377871f89c9365f56a1f82e8ce4e2
PratikK1405 <kolambkarpratik05@gmail.com>
2026-08-18 10:05:14 +0530

    Config auth-schema

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,22 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const required=[
-  ' DATABASE_URL',
-  'BETTER_AUTH_SECRET',
-  'BETTER_AUTH_URL',
+const required = [
+  " DATABASE_URL",
+  "BETTER_AUTH_SECRET",
+  "BETTER_AUTH_URL",
 ] as const;
 
-for (const key of required){
-  if (!process.env[key]){
+for (const key of required) {
+  if (!process.env[key]) {
     throw new Error(`Missing required env variable: ${key}`);
   }
 }
 
-export const env={
+export const env = {
   DATABASE_URL: process.env.DATABASE_URL!,
-  NODE_ENV: process.env.NODE_ENV||"development",
-  POST: process.env.PORT || "3000",
+  NODE_ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || "3000",
   BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET!,
   BETTER_AUTH_URL: process.env.BETTER_AUTH_URL!,
 } as const;
